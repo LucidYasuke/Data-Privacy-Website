6829dff9efdcd0d12dd291d37acc4385b9a158ab
LucidYasuke <LucidYasuke@outlook.com>
2022-02-17 10:56:26 -0500

    Commit

--- navScript.js
+++ navScript.js
@@ -2,7 +2,7 @@ var navbar = document.getElementById("navbar");
 var menu = document.getElementById("menu");
 
 window.onscroll = function(){
-    if(window.pageYOffset >= menu.offsetTop){
+    if(window.pageYOffset > menu.offsetTop){
         navbar.classList.add("sticky");
     }
     else{
